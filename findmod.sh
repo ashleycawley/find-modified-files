dbd8a6dc0b7bdb6d14b75f0316be4b40721f5a0d
Ashley Cawley <ash@ashleycawley.co.uk>
2019-02-05 12:05:59 +0000

    First revision

---
+++ findmod.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Notes
+
+# Variables
+
+# This stores script-name.sh inside the variable $SCRIPTNAME
+SCRIPTNAME=`basename "$0"`
+
+# Storing the full path and filename in a variable:
+FULLSCRIPTPATH="`pwd`/$SCRIPTNAME"
+
+# Functions
+
+# Main Script
+
+read -p "Path to check: " SUPPLIEDPATH
+
+read -p "Number of days piror: " DAYS
+
+find $SUPPLIEDPATH -mtime -$DAYS -type f -exec ls -lah {} \;
